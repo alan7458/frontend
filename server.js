3ff95841ed0ed5e2d688316baa6fc6149c224448
bmp7458 <bmp7458@programmer.net>
2025-02-09 16:09:55 -0500

    tilt.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve all files in the client directory
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Serve index.html by default
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`File server is running at http://localhost:${port}`);
+});
