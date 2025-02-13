33de6cc44f09575c863831ff90e30321ab775037
bmp7458 <bmp7458@programmer.net>
2025-02-13 11:30:06 -0500

    newserver

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 1233;
 
 // Serve all files in the client directory
 app.use(express.static(path.join(__dirname, 'client')));
